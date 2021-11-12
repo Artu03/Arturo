cc38925b7d3edb184d5d96eb6e453a3d9f086a46
Artu03 <58218409+Artu03@users.noreply.github.com>
2021-11-12 11:10:00 -0400

    Laravel

---
+++ Arturo/resources/views/formulario.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Document</title>
+</head>
+<body>
+    <div class="signin">
+
+        <form action="#" method="POST">
+        <h2 style="color:#fff;">Log In</h2>
+        <input type="text" name="username" placeholder="Username"/><br /><br />
+        <input type="password" name="password" placeholder="Password" /><br /><br />
+        <input type="submit" value="Log In" /><br /><br />
+        <div id="container">
+        <a href="#" style=" margin-right:0px; font-size:13px; font-family:Tahoma, Geneva, sans-serif;">Reset password?</a>
+            <a href="#" style=" margin-left:30px; font-size:13px; font-family:Tahoma, Geneva, sans-serif;">Forget password</a>
+            </div><br /><br /><br /><br /><br /><br />
+        Don't have account?<a href="sgnup.html"  style="font-family:'Play', sans-serif;">&nbsp;Sign Up</a>
+        
+        </form>
+        </div>
+</body>
+</html>
